56f6f1ebf2ab0e7e8e11d08a336b8240ddc25a2e
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-23 15:40:11 +0200

    feat: add ChessBoard class
    Created the ChessBoard class to represent a chess board.

---
+++ lib/board.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# class representation of a chess board
+class ChessBoard
+  attr_reader :size
+
+  def initialize(size)
+    @size = size
+  end
+
+  def valid_position?(pos_x, pos_y)
+    pos_x.between?(1..size) && pos_y.between?(1..size)
+  end
+end
