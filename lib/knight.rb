f1a1309fcdd21569b600819b392aea401877a631
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-23 17:20:13 +0200

    feat: add init method to Knight class

--- lib/knight.rb
+++ lib/knight.rb
@@ -5,15 +5,24 @@ require 'pry-byebug'
 
 # class representation of the knight chess piece
 class Knight
-  MOVES = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, 2], [1, -2], [2, -1], [2, 1]]
+  attr_accessor :position, :children
+  attr_reader :parent
+
+  MOVES = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, 2], [1, -2], [2, -1], [2, 1]].freeze
   BOARD = ChessBoard.new(8)
 
-  def generate_moves(position)
+  def initialize(position, parent = nil)
+    @position = position
+    @children = []
+    @parent = parent
+  end
+
+  def generate_moves
     moves = []
 
     MOVES.each do |move|
-      new_x = origin[0] + move[0]
-      new_y = origin[1] + move[1]
+      new_x = position[0] + move[0]
+      new_y = position[1] + move[1]
 
       moves.push([new_x, new_y]) if BOARD.valid_position?(new_x, new_y)
     end
